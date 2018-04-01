063be962b4ce071f665ee0be2ccdb51770ea7da4
unknown <kanzve@gmail.com>
2018-04-02 01:16:45 +0800

    add codes of getting metric matrix-1st version

---
+++ ad_similarity/ad_simi.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import random
+from metric_learn import ITML_Supervised
+
+def get_metric():
+    ad=pd.read_csv('ad_feature.csv',header=0)
+    data=ad.values
+    m=np.array([[.0,.0,.0,.0],[.0,.0,.0,.0],[.0,.0,.0,.0],[.0,.0,.0,.0]])
+    itml = ITML_Supervised(num_constraints=200)
+    for i in range(100):
+        data_r=np.array(random.sample(data.tolist(),int(len(data)/100)))
+        x=data_r[:,[0,2,3,4]]
+        y=data_r[:,1]
+        itml.fit(x,y)
+        m=m+itml.metric()
+    m=m/100
+    return m
+
+def output2file(metric):
+    fw=open('metric.txt')
+    for i in range(len(metric)):
+        str=metric[i][0]
+        for j in range(1,len(metric[i])):
+            str=str+'\t'+metric[i][j]
+        fw.write(str+'\n')
+    fw.close()
+    return
+
+if __name__ == '__main__':
+    m=get_metric()
+    output2file(m)

45cfcee12dec1e77eca126d978dd70b0cac48fdf
unknown <kanzve@gmail.com>
2018-04-02 16:21:21 +0800

    modify metric

--- ad_similarity/ad_simi.py
+++ ad_similarity/ad_simi.py
@@ -8,13 +8,13 @@ def get_metric():
     data=ad.values
     m=np.array([[.0,.0,.0,.0],[.0,.0,.0,.0],[.0,.0,.0,.0],[.0,.0,.0,.0]])
     itml = ITML_Supervised(num_constraints=200)
-    for i in range(100):
+    for i in range(600):
         data_r=np.array(random.sample(data.tolist(),int(len(data)/100)))
         x=data_r[:,[0,2,3,4]]
         y=data_r[:,1]
         itml.fit(x,y)
         m=m+itml.metric()
-    m=m/100
+    m=m/600
     return m
 
 def output2file(metric):

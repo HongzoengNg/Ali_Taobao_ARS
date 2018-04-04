612480775374673cd453cfa31ee6da52a357d424
unknown <kanzve@gmail.com>
2018-04-04 13:19:20 +0800

    modify distance input & output

--- ad_similarity/ad_distance.py
+++ ad_similarity/ad_distance.py
@@ -1,11 +1,13 @@
 import numpy as np
+import pandas as pd
 '''
 For this file, it provides function to calculate the similarity between advertisements
 Input:
-       X --- An sample of advertisement, in formation of array with [adgroup_id,campaign_id,customer,brand]
-       Y --- Another sample of advertisement, in formation of array with [adgroup_id,campaign_id,customer,brand]
+       X --- A dataframe of advertisements,in formation of columns with [adgroup_id,campaign_id,customer,brand]
+       Y --- Another dataframe of advertisements, in formation of columns with [adgroup_id,campaign_id,customer,brand]
 Output:
-       dist ---- A float number, the smaller it is, the higher similarity between 2 advertisements
+       dist ---- A series of float number
+                 for each of the number, the smaller it is, the higher similarity between 2 advertisements
 '''
 def get_metric():
     fr=open('metric.txt','r')
@@ -16,9 +18,17 @@ def get_metric():
     m=np.array(m)
     return m
 
-def get_dist(X,Y):
-    m=get_metric()
-    Z=np.array([X-Y])
-    dist=np.sqrt(np.dot(np.dot(Z,m),Z.T))
+def get_dist_single(x,y,M):
+    Z=np.array([x-y])
+    dist=np.sqrt(np.dot(np.dot(Z,M),Z.T))
     return dist[0][0]
 
+def get_dist_df(X,Y):
+    M=get_metric()
+    dist_lst=[]
+    for i in range(len(X)):
+        x=np.array(X.iloc[i])
+        y=np.array(Y.iloc[i])
+        dist_lst.append(get_dist_single(x,y,M))
+    dist_lst=pd.DataFrame({'distance':dist_lst})
+    return dist_lst
